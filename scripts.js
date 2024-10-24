9f7ef940e2302da630159a6b039754be775dd7bf
Kindred <kindred@kindredsalway.com>
2024-10-24 18:49:03 +0100

    update nav

--- scripts.js
+++ scripts.js
@@ -114,4 +114,14 @@ document.addEventListener("DOMContentLoaded", () => {
       closeVideoPopup();
     }
   });
+
+  const navbar = document.querySelector('nav');
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY === 0) {
+      navbar.classList.remove('scrolled');
+    } else {
+      navbar.classList.add('scrolled');
+    }
+  });
 });
